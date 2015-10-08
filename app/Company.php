7b6c2caaf8b15a03ebb75311f21ee79d28dbee7e
gavin <gavingan9e@gmail.com>
2015-10-08 06:44:58 -0400

    Added company model

---
+++ app/Company.php
@@ -0,0 +1,26 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'company';
+    /**
+     * The primary key of the table
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'company_id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'website', 'email', 'phone', 'address1', 'address2', 'city', 'state', 'country'];
+
+}
