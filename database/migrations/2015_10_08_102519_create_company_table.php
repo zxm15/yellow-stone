353c7e033c59db4f87f36d55e33a01f96e746bd6
gavin <gavingan9e@gmail.com>
2015-10-08 06:36:51 -0400

    Created company table

---
+++ database/migrations/2015_10_08_102519_create_company_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->increments('company_id');
+            $table->string('name');
+            $table->string('website');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('company');
+    }
+
+}
