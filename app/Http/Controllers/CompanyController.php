0611849220ca4290707fd28d5e4107fb705b80de
gavin <gavingan9e@gmail.com>
2015-10-08 07:17:31 -0400

    Test form facade works

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -25,7 +25,7 @@ class CompanyController extends Controller
      */
     public function create()
     {
-        //
+        return view('company.create');
     }
 
     /**
---
+++ resources/views/company/create.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+@section('content')
+<h1>Create a new company</h1>
+    {!! Form::open() !!}
+    {!! Form::close() !!}
+@endsection
