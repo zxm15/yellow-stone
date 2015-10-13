c36174a2761dd2d3c938596681d5dec8dde36e01
gavin <gavingan9e@gmail.com>
2015-10-13 06:15:13 -0400

    Created a form partial and refactor out the form from create and edit views

---
+++ resources/views/company/_form.blade.php
@@ -0,0 +1,39 @@
+<div class="form-group">
+    {!! Form::Label('name', 'Name') !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('website', 'Website') !!}
+    {!! Form::text('website', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('email', 'Email') !!}
+    {!! Form::text('email', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('phone', 'Phone') !!}
+    {!! Form::text('phone', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('address1', 'Address1') !!}
+    {!! Form::text('address1', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('address2', 'Address2') !!}
+    {!! Form::text('address2', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('city', 'City') !!}
+    {!! Form::text('city', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('state', 'State') !!}
+    {!! Form::text('state', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::Label('country', 'Country') !!}
+    {!! Form::text('country', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::submit($submitButton, ['class' => 'btn btn-primary form-control']) !!}
+</div>
--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -2,47 +2,6 @@
 @section('content')
     <h1>Create a new company</h1>
     {!! Form::open(['url' => 'company']) !!}
-    <div class="form-group">
-        {!! Form::Label('name', 'Name') !!}
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('website', 'Website') !!}
-        {!! Form::text('website', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('email', 'Email') !!}
-        {!! Form::text('email', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('phone', 'Phone') !!}
-        {!! Form::text('phone', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('address1', 'Address1') !!}
-        {!! Form::text('address1', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('address2', 'Address2') !!}
-        {!! Form::text('address2', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('city', 'City') !!}
-        {!! Form::text('city', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('state', 'State') !!}
-        {!! Form::text('state', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::Label('country', 'Country') !!}
-        {!! Form::text('country', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::submit('Add Company', ['class' => 'btn btn-primary form-control']) !!}
-    </div>
-
-
-
+        @include('company._form', ['submitButton' => 'Add company'])
     {!! Form::close() !!}
 @endsection
