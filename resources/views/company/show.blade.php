fcd61df16030fc140e2b56634234a6befde8ee3c
gavin <gavingan9e@gmail.com>
2015-10-14 07:55:44 -0400

    Fixed the information for the company show page

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>{{$company->name}} detailed info</h1>
-    @foreach($company as $key => $value)
+    @foreach($company['attributes'] as $key => $value)
         <h2 style="color:red">{{$key}}</h2>
         <h3 style="color:green">{{$value}}</h3>
     @endforeach
