a77cd01003b42a3b2d18cf7759c3fb74cda675e8
gavin <gavingan9e@gmail.com>
2015-10-15 05:30:24 -0400

    Delete a duplicate companies table and added product table migration

---
+++ database/migrations/2015_10_15_090836_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('product', function(Blueprint $table)
+		{
+			$table->increments('product_id');
+			$table->integer('company_id')->unsigned();
+			$table->foreign('company_id')->references('company_id')->on('company');
+			$table->String('name');
+			$table->String('category');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('product');
+	}
+
+}
