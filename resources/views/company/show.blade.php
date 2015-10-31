ba09178e8b0c6705ad24f6cf5153479a422a2a62
gavin <gavingan9e@gmail.com>
2015-10-31 12:35:02 -0400

    Remove company_id from displaying table

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -5,13 +5,14 @@
         <h1 style="color:darkgoldenrod">{{$company->name}} detailed info</h1>
     </div>
     @foreach($company['attributes'] as $key => $value)
-        <div class="col-md-3 col-md-offset-3 text-left">
-            <h3 style="color:darkred">{{$key}}</h3>
-        </div>
-        <div class="col-md-3 text-right">
-            <h3 style="color:darkgreen">{{$value}}</h3>
-        </div>
-
+        @unless(strcmp($key,"company_id") == 0)
+            <div class="col-md-3 col-md-offset-3 text-left">
+                <h3 style="color:darkred">{{$key}}</h3>
+            </div>
+            <div class="col-md-3 text-right">
+                <h3 style="color:darkgreen">{{$value}}</h3>
+            </div>
+        @endunless
     @endforeach
 
 @endsection
