84906b1a919f357296fcf11f4955e40f2c3a036a
gavin <gavingan9e@gmail.com>
2015-11-01 06:28:00 -0500

    Added the nav bar for welcome page but the style is off

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,53 +1,72 @@
 <html>
-	<head>
-		<title>OrderEZ</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+<head>
+    <title>OrderEZ</title>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-			.title {
-				font-size: 96px;
-				font-weight: bold;
-				margin-bottom: 40px;
-				color: goldenrod;
-			}
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-			.quote {
-				font-size: 24px;
-				font-weight: bold;
-				color: cornflowerblue;
-			}
-		</style>
-	</head>
-	<body>
+        .title {
+            font-size: 96px;
+            font-weight: bold;
+            margin-bottom: 40px;
+            color: goldenrod;
+        }
 
-		<div class="container">
-			<div class="content">
-				<div class="title">OrderEZ</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
+        .quote {
+            font-size: 24px;
+            font-weight: bold;
+            color: cornflowerblue;
+        }
+    </style>
+</head>
+<body>
+<div class="container-fluid">
+<nav class="navbar navbar-default">
+
+
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                <li><a href="{{ url('/auth/register') }}">Register</a></li>
+
+            </ul>
+        </div>
+
+</nav>
+</div>
+<div class="container">
+    <div class="content">
+        <div class="title">OrderEZ</div>
+        <div class="quote">{{ Inspiring::quote() }}</div>
+    </div>
+</div>
+
+
+</body>
 </html>
+
