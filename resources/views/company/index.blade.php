c60768e8401ea2ac2833aecf635d15837177bf35
zxm15 <gavingan9e@gmail.com>
2015-11-01 10:55:29 -0500

    Displayed the company information into a table but the style is off

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,5 +1,8 @@
 @extends('app')
 
+@section('crud')
+    <li><a href="{{action('CompanyController@create')}}">Create</a></li>
+@endsection
 @section('content')
     <div class="row text-center">
         <h1>List of companies</h1>
--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -8,17 +8,31 @@
 @section('content')
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1 style="color:darkgoldenrod">{{$company->name}} detailed info</h1>
+            <h1>{{$company->name}} detailed info</h1>
         </div>
-        @foreach($company['attributes'] as $key => $value)
-            @unless(strcmp($key,"company_id") == 0)
-                <div class="col-md-3 col-md-offset-3 text-left">
-                    <h3 style="color:darkred">{{$key}}</h3>
-                </div>
-                <div class="col-md-3 text-right">
-                    <h3 style="color:darkgreen">{{$value}}</h3>
-                </div>
-            @endunless
-        @endforeach
+        <table class="table, table-striped information-table">
+            <tbody>
+                @foreach($company['attributes'] as $key => $value)
+                    @unless(strcmp($key,"created_at") == 0 || strcmp($key, "updated_at") == 0)
+                        <tr>
+                            <th>{{$key}}</th>
+                            <th>{{$value}}</th>
+                        </tr>
+                    @endunless
+                @endforeach
+            </tbody>
+
+        </table>
+        {{--@foreach($company['attributes'] as $key => $value)--}}
+            {{--@unless(strcmp($key,"company_id") == 0)--}}
+                {{--<div class="col-md-3 col-md-offset-3 text-left">--}}
+                    {{--<h3 style="color:darkred">{{$key}}</h3>--}}
+                {{--</div>--}}
+                {{--<div class="col-md-3 text-right">--}}
+                    {{--<h3 style="color:darkgreen">{{$value}}</h3>--}}
+                {{--</div>--}}
+            {{--@endunless--}}
+        {{--@endforeach--}}
     </div>
 @endsection
+
