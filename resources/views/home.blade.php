583e6b812b2f5b055d72e9b9ef52a57a1ae3b811
gavin <gavingan9e@gmail.com>
2015-11-03 09:00:16 -0500

    Changed the brand name to product in the navigation bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 @section('crud')
     <li><a href="{{ url('company') }}">Company</a></li>
-    <li><a href="{{ url('product') }}">Brand</a></li>
+    <li><a href="{{ url('product') }}">Product</a></li>
     <li><a href="{{ url('campaign') }}">Campaign</a></li>
     <li><a href="{{ url('customer') }}">Customer</a></li>
     <li><a href="{{ url('media') }}">Media</a></li>
